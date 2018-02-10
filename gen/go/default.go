0e3b825ecb9f2e719ff55fe144d415e4b16da909
undeadops <mitch@metauser.net>
2018-02-10 17:47:19 +0000

    Generated code by SwaggerHub

---
+++ gen/go/default.go
@@ -0,0 +1,25 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Default struct {
+
+}
+
+func AddProject(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListProject(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ProjectProjectNameGet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
---
+++ gen/go/routers.go
@@ -0,0 +1,68 @@
+package 
+
+import (
+	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/undeadops/konveyer/1.0.0/",
+		Index,
+	},
+
+	Route{
+		"AddProject",
+		"POST",
+		"/undeadops/konveyer/1.0.0/project",
+		AddProject,
+	},
+
+	Route{
+		"ListProject",
+		"GET",
+		"/undeadops/konveyer/1.0.0/project",
+		ListProject,
+	},
+
+	Route{
+		"ProjectProjectNameGet",
+		"GET",
+		"/undeadops/konveyer/1.0.0/project/{projectName}",
+		ProjectProjectNameGet,
+	},
+
+}
