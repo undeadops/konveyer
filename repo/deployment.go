107719f13066dd2638edbc1ebc3ffb38c6c4a846
Mitch Anderson <mitch@undeadops.xyz>
2018-10-04 17:11:05 -0600

    Adding a write back out to files

--- repo/deployment.go
+++ repo/deployment.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 
@@ -12,7 +13,7 @@ import (
 
 // GetDeploymentImage - Read deployment file and return container names and image used
 func (repo *Repo) GetDeploymentImage(namespace, deployment string) (map[string]string, error) {
-	deployFile := fmt.Sprintf("%s/manifests/%s/%s.deploy.yaml", repo.Path, namespace, deployment)
+	deployFile := fmt.Sprintf("%s/manifests/%s/%s-deployment.yaml", repo.Path, namespace, deployment)
 
 	repo.Mutex.RLock()
 	defer repo.Mutex.RUnlock()
@@ -40,4 +41,38 @@ func (repo *Repo) GetDeploymentImage(namespace, deployment string) (map[string]s
 	}
 
 	return containers, nil
+}
+
+// SetDeploymentImage - Update Deployment file with new image, and commit it back to master
+func (repo *Repo) SetDeploymentImage(namespace, deployment string, image string) error {
+	deployFile := fmt.Sprintf("%s/manifests/%s/%s-deployment.yaml", repo.Path, namespace, deployment)
+
+	repo.Mutex.Lock()
+	defer repo.Mutex.Unlock()
+
+	ybytes, err := ioutil.ReadFile(deployFile)
+	if err != nil {
+		return err
+	}
+
+	jsonbytes, err := yaml.YAMLToJSON(ybytes)
+	if err != nil {
+		return errors.New("Error Converting Yaml to JSON from deploymentfile")
+	}
+
+	var d = appsv1.Deployment{}
+	err = json.Unmarshal(jsonbytes, &d)
+	if err != nil {
+		return errors.New("Error Unmarshaling file into Kubernetes Deployment")
+	}
+
+	// Run a split/if on image to see if I need to handle multiple containers images
+	d.Spec.Template.Spec.Containers[0].Image = image
+
+	dbytes, err := yaml.Marshal(d)
+	if err != nil {
+		return errors.New("Error Converting to byte string")
+	}
+
+	return nil
 }
